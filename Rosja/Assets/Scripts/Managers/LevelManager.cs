de455edf1b0e71c15d989bcc90500661d45b50ae
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-10-30 04:41:41 +0100

    Pause

---
+++ Rosja/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour 
+{
+    private static LevelManager _instance;
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject(typeof(LevelManager).ToString());
+                go.AddComponent<LevelManager>();
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this as LevelManager;
+       
+    }   
+    
+    private int menu = 0;
+    private int lvl = 1;
+
+
+    public void FirstLevel()
+    {
+        SceneManager.LoadScene(lvl, LoadSceneMode.Single); ; //zmienić na indeksy
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(menu, LoadSceneMode.Single);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Rosja/Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,61 @@
+﻿ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    [SerializeField]
+    private GameObject _pauseMenuUI;
+    private float _moving = 1f;
+    private float _stop = 0f;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseClick();
+
+        }
+        if (Input.GetKeyDown(KeyCode.Q)&& gameIsPaused)
+        {
+            MainMenu();
+
+        }
+    }
+    public void PauseClick()
+    {
+
+        if (gameIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            PauseOn();
+        }
+    }
+    public void Resume()
+    {
+        _pauseMenuUI.SetActive(false);
+        Time.timeScale = _moving;
+        gameIsPaused = false;
+    }
+    public void PauseOn()
+    {
+        _pauseMenuUI.SetActive(true);
+        Time.timeScale = _stop;
+        gameIsPaused = true;
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = _moving;
+        LevelManager.Instance.MainMenu();
+    }
+    public void Quit()
+    {
+        LevelManager.Instance.QuitGame();
+    }
+}
