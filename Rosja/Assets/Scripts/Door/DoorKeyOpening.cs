6c257910a66f67942a0ef5a868e8c885cfebeebb
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-11-02 19:46:09 +0100

    DoorOpeningWithKeyFailure

---
+++ Rosja/Assets/Scripts/Door/DoorKeyOpening.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKeyOpening : MonoBehaviour
+{
+
+    private bool KeyOwning = false;
+    private bool DoorkDistance = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>())
+        {
+            DoorkDistance = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>())
+        {
+            DoorkDistance = false;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && DoorkDistance && )
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
